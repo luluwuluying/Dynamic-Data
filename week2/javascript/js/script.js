959b806d46787555d6207aeab6ca54602d05f5d3
吴璐莹 <wuluying@wuluyingdeMacBook-Pro.local>
2017-01-27 22:58:49 -0500

    week 2 assignment
    Write javascript to change 3 elements' css styles

---
+++ week2/javascript/js/script.js
@@ -0,0 +1,17 @@
+document.getElementById("but").style.fontSize = "8px";
+
+
+var logos = document.getElementById("logo");
+logos.style.display = "none";
+
+//changeBackgroundColor("but", "purple");
+//
+//function changeBackgroundColor(id, color) {
+//    var currentId = document.getElementById(id);
+//    currentId.style.backgroundColor = color;
+//}
+
+document.getElementById("but").style.color ="yellow";
+
+document.getElementById("but").style.background="purple";
+
