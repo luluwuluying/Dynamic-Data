e158d9d17850795121a9e2278e8e7b99131e9939
吴璐莹 <wuluying@wuluyingdeMacBook-Pro.local>
2017-02-06 16:26:20 -0500

    functions and mouse event
    add mouse function and mouse event

--- week2/javascript/js/script.js
+++ week2/javascript/js/script.js
@@ -1,17 +1,47 @@
 document.getElementById("but").style.fontSize = "8px";
 
-
 var logos = document.getElementById("logo");
 logos.style.display = "none";
-
-//changeBackgroundColor("but", "purple");
-//
-//function changeBackgroundColor(id, color) {
-//    var currentId = document.getElementById(id);
-//    currentId.style.backgroundColor = color;
-//}
 
 document.getElementById("but").style.color ="yellow";
 
 document.getElementById("but").style.background="purple";
+
+function addElement(){
+    var parent = document.createElement("div");
+    parent.id = "logo";
+    parent.classList.add("img");
+    parent.innerHTML = "Hello Snowman";
+    document.body.appendChild(parent);
+}
+
+function mDown(obj){
+    obj.style.backgroundColor = "turquoise";
+    obj.innerHTML = "Release Me"
+}
+
+function mHover(obj){
+    obj.style.backgroundColor = "red";
+}
+
+function mOver(obj){
+    obj.style.backgroundColor ="pink";
+    obj.innerHTML = "Haha"
+}
+
+changeBackgroundColor("but1", "purple");
+changeBackgroundColor("but2","blue");
+
+function changeBackgroundColor(id, color) {
+    var currentId = document.getElementById(id);
+    currentId.style.backgroundColor = color;
+}
+
+changeText();
+
+function changeText(){
+    var h1List = document.getElementsByTagName("H1");
+    console.log(h1List.length);
+    h1List[0].innerHTML = "See you again";
+}
 
